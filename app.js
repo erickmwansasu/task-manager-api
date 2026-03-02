2e1081b2303d7cc71caf5e41466952c9d4562031
erickmwansasu <erickmwansasu.dev@gmail.com>
2026-03-02 16:49:16 +0300

    added task delete feature

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const router = express.Router()
-const cookieParser = require('cookie-parser');
-const ejs = require('ejs');
-const methodOverride = require('method-override');
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const cookieParser = require("cookie-parser");
+const ejs = require("ejs");
+const methodOverride = require("method-override");
 
-require('dotenv').config()
+require("dotenv").config();
 const PORT = process.env.PORT;
 
-const app = express()
-
-//Routes imports
-const adminRoutes = require('./routes/adminRoutes')
-const userRoutes = require('./routes/userRoutes')
+const app = express();
 
 //MongoDB Connection
-const dbURI = 'mongodb://localhost:27017/taskmanagerapi3'
-mongoose.connect(dbURI)
+const dbURI = "mongodb://localhost:27017/taskmanagerapi3";
+mongoose.connect(dbURI);
 
 //Middleware to accept JSON
-app.use(express.json())
-app.use(methodOverride('_method'))
+app.use(express.json());
+app.use(methodOverride("_method"));
 
 //Middleware to serve static files
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //Middleware to accept/handle form data
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 //Middleware for cookies
-app.use(cookieParser())
+app.use(cookieParser());
 
 //Set view engine
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+
+//Routes imports
+const adminRoutes = require("./routes/adminRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 //Routes
-app.use(adminRoutes)
-app.use(userRoutes)
+app.use(adminRoutes);
+app.use(userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}.`)
-})
+  console.log(`Server is running on PORT ${PORT}.`);
+});
